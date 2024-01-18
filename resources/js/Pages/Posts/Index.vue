19a6465b2a2e84f0972ef025c25a4149184d9ce8
veljko146 <veljko146@gmail.com>
2024-01-18 04:33:47 +0100

    first commit

---
+++ resources/js/Pages/Posts/Index.vue
@@ -0,0 +1,21 @@
+<template>
+    <AppLayout>
+        <!-- Your page content goes here -->
+        Hello World!
+    </AppLayout>
+</template>
+
+<script>
+import AppLayout from '@/Layouts/AppLayout.vue'; // Adjust the path as necessary
+
+export default {
+    components: {
+        AppLayout
+    },
+
+};
+</script>
+
+<style>
+
+</style>
